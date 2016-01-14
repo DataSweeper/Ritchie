fc7cc4e2c4b6eef79727489606a734d3c9672d3f
sivagnanam <gnanamsiva@outlook.com>
2016-01-14 20:22:10 +0530

    lispy

--- buildourownlisp/lispy/prompt.c
+++ buildourownlisp/lispy/prompt.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#ifdef _WIN32
+#include <string.h>
 
-int main (int argc, char** argv)
+static char buffer[2048];
+
+char* readline (char* prompt)
 {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
+}
 
-	puts("Lispy Version 0.0.0.0");
-	puts("Press Ctrl+c to Exit");
+void add_histroy (char* unused) {}
 
-	while (1) {
-		fputs("lispy>", stdout);
-		fgets(input, 2048, stdin);
-		printf("No you are a %s", input);
-	}
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
-	return 0;
+int main(int argc, char** argv)
+{
+	puts("Lispy Version 0.0.0.0.1");
+  	puts("Press Ctrl+c to Exit\n");
+   
+  	while (1) {
+		char* input = readline("lispy> ");
+    	add_history(input);
+    	printf("No you're a %s\n", input);
+    	free(input);
+  	}
+  
+  	return 0;
 }
